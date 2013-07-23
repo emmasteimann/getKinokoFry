de7e286fd172764ac4ecadfd6b1e61662c46779c
emmasteimann <emmasteimann@gmail.com>
2013-07-23 13:28:15 -0500

    Created downloader for Kinoko Fry.

---
+++ Readme.md
@@ -0,0 +1,21 @@
+Kinoko Fry Downloader
+=========
+
+Purpose....
+-----------
+I made my original comic downloader for Scary Go Round, but then I wondered if I could expand it to my other favorite web comics, I originally followed up with [Okay Pants](http://web.archive.org/web/20070620013447/http://www.okaypants.com/comic.php) and got that to work, so I've now expanded it to [KinokoFry](http://kinokofry.com).
+
+Each chapter is created every 25 comics and loaded into its own `.cbz` file. Which can be used with most comic readers. For Mac, see [Simple Comic](http://dancingtortoise.com/simplecomic/) or [Jomic](http://jomic.sourceforge.net/).
+
+How to use...
+-----------
+A downloader tool for my favorite webcomic.
+
+  - Run `bundle`
+  - Run `bundle exec ruby getKinokoFry.rb`
+  - Enjoy all of KinokoFry!
+
+What this does....
+-----------
+
+This downloads Kinoko Fry (not Secret Mystery Diary or BEC I think... havent verified) from 2008-Present, where recently Rebecca Clements has taken a turn away from mushrooms to interesting almost Dr. Seuess-esque drawings.
---
+++ getKinokoFry.rb
@@ -0,0 +1,94 @@
+require 'mechanize'
+require 'logger'
+require 'pry'
+require 'zip/zip'
+
+class GetKinokoFry
+  attr_accessor :agent, :first_page, :chapter_markers, :previous_url
+
+  def initialize()
+    @agent = Mechanize.new
+    @agent.log = Logger.new "mech.log"
+    @agent.user_agent_alias = 'Mac Safari'
+  end
+
+  def start_download
+    # Start with first page of first Chapter
+    page = @agent.get("http://kinokofry.com/archive/kinokofry-007/")
+    puts @agent.current_page().uri()
+
+    keep_going = true
+    current_chapter = 1
+    page_cursor = 1
+    while keep_going
+      page = @agent.current_page()
+      if page_cursor % 25 == 0
+        zip_previous_chapter(current_chapter)
+        current_chapter = current_chapter + 1
+        puts "Chapter switched to -> " + current_chapter.to_s
+      end
+      comic_url = page.search("//img[contains(@src,'webcomic')]").first.attributes["src"].to_s
+      image_name = comic_url.split('/')[-1]
+      @agent.get("#{comic_url}").save("#{chapter_directory(current_chapter)}/#{image_name}")
+      puts "Currently Downloading: #{current_chapter}"
+      puts "Downloading comic address: #{comic_url}"
+
+      next_link = page.link_with(:text => /Next.*/)
+      if next_link
+        next_url = next_link.href
+        if @previous_url == next_url
+          comic_url = page.search("//img[contains(@src,'webcomic')]").first.attributes["src"].to_s
+          image_name = comic_url.split('/')[-1]
+          @agent.get("#{comic_url}").save("#{chapter_directory(current_chapter)}/#{image_name}")
+          puts "Currently Downloading: #{current_chapter}"
+          puts "Downloading comic address: #{comic_url}"
+          zip_previous_chapter(current_chapter)
+          keep_going = false
+        end
+        next_page = @agent.get(next_url)
+        @previous_url = next_url
+      else
+        zip_previous_chapter(current_chapter)
+        keep_going = false
+      end
+      page_cursor = page_cursor + 1
+    end
+  end
+
+  def chapter_directory(chapter_id)
+    "kinokofry_comics/chapter_#{chapter_id}"
+  end
+
+  def zip_file_path(chapter_id)
+    directory_name = "saved_comics"
+    unless File.directory?(directory_name)
+      FileUtils.mkdir_p(directory_name)
+    end
+    "#{directory_name}/chapter_#{chapter_id}.cbz"
+  end
+
+  def zip_previous_chapter(chapter_id)
+    directory = chapter_directory(chapter_id)
+    zipfile_name = zip_file_path(chapter_id)
+    if File.exist?(zipfile_name)
+      File.delete(zipfile_name)
+    end
+    Zip::ZipFile.open(zipfile_name, 'w') do |zipfile|
+      Dir["#{directory}/**/**"].reject{|f|f==zipfile_name}.each do |file|
+        zipfile.add(file.sub(directory+'/',''),file)
+      end
+    end
+  end
+
+  class << self
+    def new_download()
+      scary_downloader = self.new()
+      scary_downloader.start_download
+    end
+  end
+
+end
+
+GetKinokoFry.new_download()
+
+exit
